23c592700f18552a1b1d622f628ff33677cfde6f
itshirdeshk <hirdeshkhandelwal58@gmail.com>
2025-07-03 18:41:25 +0530

    feat: add general routes

--- routes/GeneralRoutes.js
+++ routes/GeneralRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { getAllTerms } from '../controllers/TermsController.js';
+import { getAllFAQs } from '../controllers/FAQController.js';
+import { getAllPrivacyPolicies } from '../controllers/PrivacyPolicyController.js';
+import { getAllAboutUs } from '../controllers/AboutUsController.js';
 
 const router = express.Router();
 
-router.get('/example', (req, res) => {
-    res.status(200).json({ message: "Example route" });
-});
+router.get('/all/faq', getAllFAQs);
+router.get('/all/terms', getAllTerms);
+router.get('/all/privacy-policy', getAllPrivacyPolicies);
+router.get('/all/about-us', getAllAboutUs);
 
 export default router;
